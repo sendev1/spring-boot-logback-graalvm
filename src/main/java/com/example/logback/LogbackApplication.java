0e4a67cf8bfe5bc5dc4e74bcbcc523ba6ca97fec
Kris Foster <me@krismade.me>
2021-02-16 12:43:52 +0000

    Working

--- src/main/java/com/example/logback/LogbackApplication.java
+++ src/main/java/com/example/logback/LogbackApplication.java
@@ -1,13 +1,19 @@
 package com.example.logback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class LogbackApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(LogbackApplication.class);
+
 	public static void main(String[] args) {
+		logger.error("START This is NOT an error message......");
 		SpringApplication.run(LogbackApplication.class, args);
+		logger.error("AFTER INIT This is NOT an error message......");
 	}
 
 }
